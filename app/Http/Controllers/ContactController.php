61cb93df40ddbd496ff15a423e2ec852dfb0dc94
hendisantika <hendisantika@yahoo.co.id>
2023-12-23 06:27:57 +0700

    Modify ContactController.php 🫸🌀✏️📗 :octocat:

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('contact');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'pesan' => 'required',
+            'g-recaptcha-response' => 'required|recaptchav3:register,0.5'
+        ]);
+
+        dd('ok!');
+    }
 }
